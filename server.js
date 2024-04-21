8ad2630b9d7d9e56283761d2b265b6595ec26007
Damir Yunusov <damuraiz@MacBook-Air-Damir.local>
2024-04-22 00:18:17 +0300

    server

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const axios = require('axios');
-const https = require('https');
-const fs = require('fs');
+
+
 const app = express();
 
 app.use(express.json());
 
-const c = Buffer.from(process.env.CERT_BASE, 'base64');
-const k = Buffer.from(process.env.KEY_BASE, 'base64');
-
-fs.writeFileSync('key.pem', k);
-fs.writeFileSync('cert.pem', c);
-
-const httpsOptions = {
-  cert: fs.readFileSync('cert.pem'),
-  key: fs.readFileSync('key.pem')
-};
-
-
-
-app.all('*', async (req, res) => {
-  const targetUrl = 'https://api.themoviedb.org' + req.originalUrl; // Используйте HTTPS в URL
+app.get('*', async (req, res) => {
+  const targetUrl = 'https://api.themoviedb.org' + req.originalUrl;
   
   try {
     const response = await axios.get(targetUrl);
-//    console.log('Status:', response.status);
     console.log('Data:', response.data);
 	res.status(response.status).send(response.data);
     
@@ -32,12 +18,10 @@ app.all('*', async (req, res) => {
     console.log('Error status:', error.response?.status);
     console.log('Error data:', error.response?.data);
 	res.status(error.response?.status || 500).send(error.message);
-   // throw error;  // Выкидываем ошибку дальше, чтобы можно было её обработать
   }
-  
-});
+}); 
 
-const PORT = 3000;
-https.createServer(httpsOptions, app).listen(PORT, () => {
-  console.log(`HTTPS сервер запущен на порту ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
