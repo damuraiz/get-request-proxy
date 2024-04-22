cf8210d689d1013ef248fcf7e91280d860d11b15
Damir Yunusov <damuraiz@MacBook-Air-Damir.local>
2024-04-22 21:43:53 +0300

    server

--- server.js
+++ server.js
@@ -16,34 +16,44 @@ app.get('*', async (req, res) => {
     // Проверка расширения запрашиваемого файла
     if (/\.(jpg|jpeg|png|svg|ico)$/i.test(req.path)) {
 		targetUrl = STATIC_BASE_URL + req.originalUrl;
-    } else {
-      	targetUrl = API_BASE_URL + req.originalUrl;
-    }
-	console.log(targetUrl);
-	try {
-		console.log(req.method);
-		const response = await axios({
-		      method: req.method,
-		      url: targetUrl,
-//		      headers: { ...req.headers, host: new URL(targetUrl).hostname },
-//		      httpsAgent: new https.Agent({
-//		        rejectUnauthorized: false // Указывается для разработки, в продакшене должно быть true
-//		      }),
-		      responseType: 'arraybuffer' // сохраняем бинарный ответ, если это изображение или другой бинарный контент
-		    });
+		try {
+			console.log(req.method);
+			const response = await axios({
+			      method: req.method,
+			      url: targetUrl,
+	//		      headers: { ...req.headers, host: new URL(targetUrl).hostname },
+	//		      httpsAgent: new https.Agent({
+	//		        rejectUnauthorized: false // Указывается для разработки, в продакшене должно быть true
+	//		      }),
+			      responseType: 'arraybuffer' // сохраняем бинарный ответ, если это изображение или другой бинарный контент
+			    });
 
-		    // Транслируем хедеры ответа
-		    res.set(response.headers);
+			    // Транслируем хедеры ответа
+			    res.set(response.headers);
 
-		    // Отправляем статус и данные ответа
-		    res.status(response.status).send(response.data);
+			    // Отправляем статус и данные ответа
+			    res.status(response.status).send(response.data);
 
       
-  	} catch (error) {
-    	console.log('Error status:', error.response?.status);
-    	console.log('Error data:', error.response?.data);
-		res.status(error.response?.status || 500).send(error.message);
-  	}
+	  	} catch (error) {
+	    	console.log('Error status:', error.response?.status);
+	    	console.log('Error data:', error.response?.data);
+			res.status(error.response?.status || 500).send(error.message);
+	  	}
+    } else {
+      	targetUrl = API_BASE_URL + req.originalUrl;
+		try {
+	    	const response = await axios.get(targetUrl);
+//	    	console.log('Data:', response.data);
+			res.status(response.status).send(response.data);
+    
+	  	} catch (error) {
+//	    	console.log('Error status:', error.response?.status);
+//	    	console.log('Error data:', error.response?.data);
+			res.status(error.response?.status || 500).send(error.message);
+	  	}
+    }
+	console.log(targetUrl);
 }); 
 
 const PORT = process.env.PORT || 3000;
